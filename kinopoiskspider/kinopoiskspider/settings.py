d137d4f5a0852a9d20b1dcc484fbcb1cb8e2c4f4
Ilyas Podprugin <p0dprugin.ilia@gmail.com>
2023-01-05 05:05:54 +0300

    Added DOWNLOADER_MIDDLEWARES in settings.py to fake user-agent

--- kinopoiskspider/kinopoiskspider/settings.py
+++ kinopoiskspider/kinopoiskspider/settings.py
@@ -50,9 +50,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    'kinopoiskspider.middlewares.KinopoiskspiderDownloaderMiddleware': 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'scrapy_fake_useragent.middleware.RandomUserAgentMiddleware': 400,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
